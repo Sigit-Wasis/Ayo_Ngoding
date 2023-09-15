4eab76c5cc1aae23da1776adb55294cdbd854103
Dhitahumdana <dhitahumdana@gmail.com>
2023-09-15 11:46:33 +0700

    komentar

---
+++ Dhita/php/belajar1.php
@@ -0,0 +1,55 @@
+<?php 
+ echo "My Internship";
+ echo "<br/>";
+ echo "<hr/>";
+
+// OPERATORS
+$angka1 = 10;
+$angka2 = 20;
+
+// PENJUMLAHAN
+echo "penjumlahan 10 + 20 = ";
+echo $angka1 + $angka2;
+
+echo "<br/>";
+echo "<hr/>";
+
+
+// PERKALIAN 
+echo "perkalian 10 * 20 = ";
+echo $angka1 * $angka2;
+
+echo "<br/>";
+echo "<hr/>";
+
+
+// PENGURANGAN
+echo "pengurangan 10 - 20 = ";
+echo $angka1 - $angka2;
+
+echo "<br/>";
+echo "<hr/>";
+
+
+// PEMBAGIAN
+echo "pembagian 10 / 20 = ";
+echo $angka1 / $angka2;
+
+echo "<br/>";
+echo "<hr/>";
+
+// PEMANGKATAN
+echo "pemangkatan 10 ** 20 = ";
+echo $angka1 ** $angka2;
+
+echo "<br/>";
+echo "<hr/>";
+
+// SISA BAGI
+echo "sisa bagi 10 % 20 = ";
+echo $angka1 % $angka2;
+
+echo "<br/>";
+echo "<hr/>";
+
+?>
