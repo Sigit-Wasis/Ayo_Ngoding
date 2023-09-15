075de239f3ba58e01b1a6b0a8c23930f91ada4b6
CahyoAnom <cahyoanom29@gmail.com>
2023-09-15 10:08:28 +0700

    komentar

---
+++ Cahyo/php/index.php
@@ -0,0 +1,31 @@
+<?php
+//menampilkan kata hallo
+echo "Hallo";
+
+//tampilkan variabel untuk menyimpan data baik itu string, integer dll
+// tampilkan variabel namaku
+$namaku = " Cahyo Anom ";
+echo $namaku;
+echo "</br/>";
+
+$huruf = " Microdata";
+$angka = 100;
+
+$angka1 = 40;
+$angka2 = 30;
+
+//jumlahkan angka1 dan angka2
+echo $angka1 + $angka2;
+echo "</br/>";
+
+echo $huruf;
+echo "</br/>";
+echo $angka;
+echo "</br/>";
+
+//KONSTANTA
+const nama = "ANJAY";
+echo nama;
+echo "</br/>";
+
+?>
