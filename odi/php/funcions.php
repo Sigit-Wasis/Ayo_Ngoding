531b880daf03396c1f7ce6f0bc87c4ef03e741a7
odiadrian <adrianaodi592@gmail.com>
2023-09-18 15:24:44 +0700

    funcions

--- odi/php/funcions.php
+++ odi/php/funcions.php
@@ -2,15 +2,13 @@
 
 function luasBelahKetupat($diagonal1, $diagonal2) {
     // Menghitung luas belah ketupat
-    $luas = ($diagonal1 * $diagonal2 / 2) ;
+    $luas = ($diagonal1 * $diagonal2 * 0.5) ;
     return $luas;
 }
-
 
 // Contoh penggunaan fungsi
 $diagonal1 = 8;
 $diagonal2 = 6;
-
 
 echo "Luas belah ketupat dengan diagonal $diagonal1 dan $diagonal2 adalah: " . luasBelahKetupat($diagonal1, $diagonal2,) . "<br>";
 
