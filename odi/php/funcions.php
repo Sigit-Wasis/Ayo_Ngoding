378e83c9899fff0df52de66868f4cfcdd1ea1fa0
odiadrian <adrianaodi592@gmail.com>
2023-09-18 15:47:43 +0700

    funcions

--- odi/php/funcions.php
+++ odi/php/funcions.php
@@ -12,7 +12,7 @@ function Luaspersegi() {
   $keliling_persegi = 4 * $sisi;
   $luas_persegi = $sisi * $sisi;
    
-  echo "Panjang sisi persegi = $sisi <br>";
+  echo "sisi persegi = $sisi <br>";
   echo "Keliling persegi = (4 x $sisi) = $keliling_persegi <br>";
   echo "Luas persegi = ($sisi x $sisi) = $luas_persegi <br>";
 
