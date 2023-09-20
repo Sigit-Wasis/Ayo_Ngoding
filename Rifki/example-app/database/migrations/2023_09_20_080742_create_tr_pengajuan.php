752e74e631a59e6b8a0a1f63b03ac775f7881e7b
RifkiAlvareza12 <rifkialva040901@gmail.com>
2023-09-20 02:01:29 -0700

    belajar laravel dasar

---
+++ Rifki/example-app/database/migrations/2023_09_20_080742_create_tr_pengajuan.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tr_pengajuan', function (Blueprint $table) {
+            $table->id();
+            $table->string('tanggal_pengajuan');
+            $table->string('grand_total');
+            $table->string('status_pengajuab_ap');
+            $table->string('keterangan_ditolak_ap');
+            $table->string('status_pengajuan_vendor');
+            $table->foreignId('created_by')->notNull()->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('upadated_by')->notNull()->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tr_pengajuan');
+    }
+};
