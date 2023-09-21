226ae02b63ea3c83dfbd16f9bdb5422a519a9007
RifkiAlvareza12 <rifkialva040901@gmail.com>
2023-09-21 02:32:20 -0700

    belajar membuat database di laravel

---
+++ Rifki/example-app/app/Models/JenisBarang.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisBarang extends Model
+{
+    use HasFactory;
+    // NAMA TABLE SAMA DENGAN YANG ADA DI DATABASE
+    protected $table = 'mst_jenis_barang';
+    // SEMUA FIELD ITU TERISI
+    protected $guarded =[];
+}
