1792f8c3adf30735f71d8e0e149014e511140c7c
odiadrian <adrianaodi592@gmail.com>
2023-09-26 16:35:05 +0700

    Membuat Update,Panination,Timezone Pada Jenis Barang

---
+++ odi/example-app/app/Http/Requests/JenisBarangRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class JenisBarangRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama_jenis_barang' => 'required|max:255',
+            'deskripsi' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nama_jenis_barang.required' => 'Nama Jenis Barang Wajib Diisi',
+            'nama_jenis_barang.max' => 'Nama Jenis Barang Tidak Boleh Banyak',
+            'deskripsi.required' => 'Deskripsi Barang Wajib Diisi',
+        ];
+    }
+}
