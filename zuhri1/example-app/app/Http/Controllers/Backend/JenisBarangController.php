fec07ff1a93a701d946c5321c369554d6794547a
sitiaminatuzuhriyah <zuhriyahzuhri94@gmail.com>
2023-09-26 00:34:45 -0900

    belajar membuat fitur update,pagination,timezone jenis barang

--- zuhri1/example-app/app/Http/Controllers/Backend/JenisBarangController.php
+++ zuhri1/example-app/app/Http/Controllers/Backend/JenisBarangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\JenisBarangRequest;
 use Illuminate\Http\Request;
 use DB;
 
@@ -12,15 +13,18 @@ class JenisBarangController extends Controller
 //query ini untuk mengambil data jenis barang secara keseluruhan dengan id secara discending
         $jenisBarang = DB::table('jenis_barang')->select('jenis_barang.*', 'name as created_by')->orderBy('jenis_barang.id','DESC')
  ->join('users','users.id','jenis_barang.created_by')
- ->get();
+ ->paginate(3);
 
         return view('backend.jenis_barang.index',compact('jenisBarang'));
     }
 
     public function create () {
         return view ('backend.jenis_barang.create');
+        //TIPE DATA $Request adalah object
+        //DD (die dump untuk memeriksa apakah ada value atau record didalam variabel $request yang diambil dari from inputan )
+        // dd($request->all());
     }
-    public function store(Request $request) {
+    public function store(JenisBarangRequest $request) {
         DB::table('jenis_barang')->insert([
             'nama_jenis_barang' => $request->nama_jenis_barang,
             // 'deskripsi' =>$request->deskripsi,
@@ -34,6 +38,27 @@ class JenisBarangController extends Controller
 
         return redirect()->route('jenis_barang')->with('message','Jenis Barang Berhasil Disimpan!');
     }
+
+    public function edit($id){
+        //apa tipe data dari $id ?
+        //menggunakan first karena kita mau ngambil datamhanya 1 yang sesuai dengan ID
+        $editJenisBarang = DB::table('jenis_barang')->where('id',$id)->first();
+
+        return view('backend.jenis_barang.edit',compact('editJenisBarang'));
+    }
+
+    public function update(JenisBarangRequest $request, $id) {
+        DB::table('jenis_barang')->where('id', $id)->update([
+            'nama_jenis_barang' => $request->nama_jenis_barang,
+            // 'deskripsi' =>$request->deskripsi,
+            'deskripsi' => $request->deskripsi,
+            'updated_by' => 1,
+            'updated_at' => \Carbon\Carbon::now(),
+        ]);
+
+        return redirect()->route('jenis_barang')->with('message','Jenis Barang Berhasil Disimpan!');
+    }
+
     public function destroy($id) {
         DB::table('jenis_barang')->where('id',$id)->delete();
      
