176005e32279394ac358388e3b80b6861e444a8b
CahyoAnom <cahyoanom29@gmail.com>
2023-09-27 16:55:11 +0700

    Belajar membuat login/autantication, logout, dan menambah data user

--- Cahyo/example-app/routes/web.php
+++ Cahyo/example-app/routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('backend.home.');
-// });
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 Route::group(['namespace' => 'App\Http\Controllers'], function() {
-    Route::get('/', 'Backend\BerandaController@index')->name('beranda');
+    Route::get('/home', 'Backend\BerandaController@index')->name('beranda');
     
     Route::get('/jenis_barang','Backend\JenisBarangController@index')->name('jenis_barang');
     Route::get('/tambah_jenis_barang', 'Backend\JenisBarangController@create')->name('tambah_jenis_barang');
@@ -27,4 +27,15 @@ Route::group(['namespace' => 'App\Http\Controllers'], function() {
     Route::put('/jenis_barang/update/{id}', 'Backend\JenisBarangController@update')->name('jenis_barang.update');
     Route::get('/delete_jenis_barang/{id}','Backend\JenisBarangController@destroy')->name('delete_jenis_barang');
     
+    // Route untuk user
+    Route::get('/user','Backend\userController@index')->name('user');
+    Route::get('/tambah_user', 'Backend\userController@create')->name('tambah_user');
+    Route::post('/store_user', 'Backend\userController@store')->name('store_user');
+    Route::get('/edit_user/{id}', 'Backend\userController@edit')->name('edit_user');
+    Route::post('/update_user/{id}', 'Backend\userController@update')->name('update_user');
+    Route::get('/delete_user/{id}','Backend\userController@destroy')->name('delete_user');
+    
 });
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
