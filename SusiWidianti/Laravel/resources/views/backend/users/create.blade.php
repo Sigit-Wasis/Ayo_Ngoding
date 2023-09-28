a7508524db4b69e23ea4fdbd58449a5b4072078d
SusiWidian12 <widiantisusi97@gmail.com>
2023-09-28 16:26:32 +0700

    belajar membuat edit dan update users

---
+++ SusiWidianti/Laravel/resources/views/backend/users/create.blade.php
@@ -0,0 +1,77 @@
+@extends('backend.app')
+
+@section('content')
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>User</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">User</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- KONTEN TAMBAH USER -->
+    <section class="content">
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form method="POST" action="{{ route('store_users') }}">
+            @csrf
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" value="{{old('username') }}"  id="username" name="username" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" value="" id="password" name="password" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="nama_lengkap">Nama Lengkap</label>
+                    <input type="text" class="form-control" value="{{old('nama_lengkap') }}"  id="nama_lengkap" name="nama_lengkap" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <input type="text" class="form-control" value="{{old('alamat') }}" id="alamat" name="alamat" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="nomor_telepon">Nomor Telepon</label>
+                    <input type="text" class="form-control" value="{{old('nomor_telepon') }}" id="nomor_telepon" name="nomor_telepon" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" value="{{old('email') }}" id="email" name="email" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="password_confirmation">Konfirmasi Password</label>
+                    <input type="password" class="form-control" value="" id="password_confirmation" name="password_confirmation" placeholder="">
+                </div>
+                
+            </div>
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Simpan User</button>
+                <a href="{{ route('users') }}" class="btn btn-info">Kembali</a>
+            </div>
+        </form>
+    </section>
+</div>
+
+@endsection
