7e5642f895e68e35e4d0db8dc364ee353b77c557
SfynHdi <sofyanuap@gmail.com>
2023-10-02 17:24:49 +0700

    belajar dasar laravel belajar membuat crud data barang

---
+++ sofyan/example-app/resources/views/backend/data_barang/index.blade.php
@@ -0,0 +1,103 @@
+@extends('backend.app')
+@section('content')
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Data Barang</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">data barang</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="card-footer clearfix">
+        <a href="{{ route ('tambah-barang') }}" class="btn btn-primary float-right"><i class="fas fa-plus"></i> Tambah Barang</a>
+    </div>
+    <section class="content">
+        <div class="card">
+            <div class="card-body">
+                @if(Session::has('message'))
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h5>
+                        <i class="icon fas fa-check"></i> Sukses!
+                    </h5>
+                    {{ Session('message')}}
+                </div>
+                @endif
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">NO</th>
+                            <th scope="col">Jenis Barang</th>
+                            <th scope="col">Kode Barang</th>
+                            <th scope="col">Nama Barang</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col">Satuan</th>
+                            <th scope="col">Deskripsi</th>
+                            <th scope="col">Stok</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Di Buat Pada</th>
+                            <th scope="col">Di Buat Oleh</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{-- Calculate the row number --}}
+                        @php
+                        $rowNumber = ($dataBarang->currentPage() - 1) * $dataBarang->perPage() + 1;
+                        @endphp
+
+                        @forelse($dataBarang as $data)
+                        <tr>
+                            <td scope="row">{{ $rowNumber++ }}</td>
+                            <td>{{$data->jenis_barang}}</td>
+                            <td>{{$data->kode_barang}}</td>
+                            <td>{{$data->nama_barang}}</td>
+                            <td>{{$data->harga}}</td>
+                            <td>{{$data->satuan}}</td>
+                            <td>{{$data->deskripsi}}</td>
+                            <td>{{$data->stok}}</td>
+                            <td>
+                                @if(!empty($data->image))
+                                <img src="{{ asset('assets/dist/img/' . $data->image) }}" alt="" class="img-thumbnail" width="100">
+                                @else
+                                No Image
+                                @endif
+                            </td>
+                            <td>{{$data->created_at ?? \Carbon\Carbon::now() }}</td>
+                            <td>{{$data->created_by}}</td>
+                            <td>
+                                <a href="" class="btn btn-sm btn-primary">Edit</a>
+                                <a href="{{ route('delete_barang', ['id' => $data->id]) }}" onclick="return confirm('are you sure?')" class="btn btn-sm btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        @empty
+                        <!-- Tidak ada data barang -->
+                        @endforelse
+                    </tbody>
+                </table>
+
+                @if($dataBarang->isEmpty())
+                <p class="text-center">Tidak Ada Data Barang</p>
+                @endif
+
+                <div class="float-right">
+                    {{ $dataBarang->links() }}
+                </div>
+
+            </div>
+        </div>
+    </section>
+</div>
+
+@endsection
