9bd4390f65b41708c34708375b7aef7f26b3831b
odiadrian <adrianaodi592@gmail.com>
2023-10-12 16:58:06 +0700

    add fitur pengajuan barang

--- odi/example-app/app/Http/Controllers/Backend/PengajuanController.php
+++ odi/example-app/app/Http/Controllers/Backend/PengajuanController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\DB as FacadesDB;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -61,10 +63,59 @@ class PengajuanController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) 
+    { 
+        // DB::beginTransaction(); 
+ 
+        // try { 
+            
+            // Insert ke tr_pengajuan 
+            $id_tr_pengajuan = DB::table('pengajuan')->insertGetId([ 
+                'tanggal_pengajuan' => $request->tanggal_pengajuan, 
+                'grand_total' => 0, 
+                'status_pengajuan_ap' => 0, 
+                'keterangan_ditolak_ap' => '', 
+                'status_pengajuan_vendor' => 0, 
+                'keterangan_ditolak_vendor' => '', 
+                'created_by' => Auth::user()->id, 
+                'updated_by' => Auth::user()->id, 
+                'created_at' => \Carbon\Carbon::now(), 
+                'updated_at' => \Carbon\Carbon::now(), 
+            ]); 
+ 
+            $grandTotal = 0; 
+ 
+            $countData = count($request->id_barang); 
+            for ($i=0; $i < $countData; $i++) {  
+                DB::table('detail_pengajuan')->insert([ 
+                    'id_barang' => $request->id_barang[$i], 
+                    'jumlah' => $request->jumlah_barang[$i], 
+                    'id_tr_pengajuan' => $id_tr_pengajuan, 
+                    'total_per_barang' => $request->jumlah_barang[$i] * $request->harga_barang[$i],
+                    'created_by' => Auth::user()->id, 
+                    'updated_by' => Auth::user()->id,  
+                    'created_at' => \Carbon\Carbon::now(), 
+                    'updated_at' => \Carbon\Carbon::now(), 
+                ]); 
+            // UPDATE STOK BARANG 
+            DB::table('barang')->where('id', $request->id_barang[$i])->decrement('stok', $request->jumlah_barang[$i]);
+                $grandTotal += $request->jumlah_barang[$i] * $request->harga_barang[$i]; 
+            } 
+ 
+            DB::table('pengajuan')->where('id', $id_tr_pengajuan)->update([ 
+                'grand_total' => $grandTotal 
+            ]); 
+             
+            // DB::commit(); 
+ 
+            return redirect()->route('pengajuan')->with('message', 'Pengajuan Berhasil Diajukan'); 
+            // all good 
+        // } catch (\Exception $e) { 
+        //     DB::rollback(); 
+        //     // something went wrong 
+        // } 
     }
+    
 
     /**
      * Display the specified resource.
@@ -95,6 +146,8 @@ class PengajuanController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        FacadesDB::table('pengajuan')->where('id', $id)->delete();
+
+        return redirect()->route('pengajuan')->with('message','Data Pengajuan Berhasil Dihapus');
     }
 }
