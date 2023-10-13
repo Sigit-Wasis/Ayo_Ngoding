dcf37d00a67c7b596efec600421f173c7303323f
SfynHdi <sofyanuap@gmail.com>
2023-10-13 17:01:39 +0700

    belajar dasar laravel add fitur penerimaan dan penolakan perizinan

--- sofyan/example-app/app/Http/Controllers/Backend/TransaksiBarangController.php
+++ sofyan/example-app/app/Http/Controllers/Backend/TransaksiBarangController.php
@@ -19,7 +19,7 @@ class TransaksiBarangController extends Controller
             ->select(
                 '_t_r__pengajuan.*',
                 'users.name as created_by',
-                
+
 
             )
             ->orderBy('_t_r__pengajuan.id', 'DESC')
@@ -29,109 +29,7 @@ class TransaksiBarangController extends Controller
         return view('backend.tr_pengajuan.index', compact('trPengajuan'));
     }
 
-    // public function createPengajuan()
-    // {
-    //     // Ambil daftar vendor
-    //     $vendors = DB::table('vendors')->get();
-
-    //     // Ambil daftar barang yang sesuai dengan vendor yang dipilih
-    //     $barangPerVendor = [];
-
-    //     foreach ($vendors as $vendor) {
-    //         $barang = DB::table('_m_s_t__barang')
-    //             ->where('vendor_id', $vendor->id)
-    //             ->get();
-
-    //         $barangPerVendor[$vendor->id] = $barang;
-    //     }
-
-    //     // Mengambil data harga dan satuan untuk setiap barang
-    //     $dataHargaSatuan = [];
-
-    //     foreach ($barangPerVendor as $vendorId => $barangList) {
-    //         foreach ($barangList as $barang) {
-    //             $barangData = DB::table('_m_s_t__barang')
-    //                 ->select('harga', 'stok', 'image')
-    //                 ->where('id', $barang->id)
-    //                 ->first();
-
-    //             $dataHargaSatuan[$vendorId][$barang->id] = [
-    //                 'harga' => $barangData->harga,
-    //                 'stok' => $barangData->stok,
-    //                 'image' => $barangData->image,
-    //             ];
-    //         }
-    //     }
-
-    //     return view('backend.tr_pengajuan.create', compact('vendors', 'barangPerVendor', 'dataHargaSatuan'));
-    // }
-
-
-
-    //tipe data request adalah object
-    //DD (die dump untuk memeriksa apakah ada value atau record di dalam variabel $request yang di amabil dari form imputan)
-    //dd($request->all());
-
-    // public function storePengajuan(TrPengajuanRequest $request)
-    // {
-    //     // Kode Anda yang ada untuk menyimpan catatan pengajuan utama di sini...
-    //     // Simpan data pengajuan ke dalam tabel _t_r__pengajuan
-    //     $trPengajuanId = DB::table('_t_r__pengajuan')->insertGetId([
-    //         'tanggal_pengajuan' => $request->tanggal_pengajuan,
-    //         'grand_total' => 0, // Default grand total
-    //         'status_pengajuan_ap' => 1, //$request->status_pengajuan_ap,
-    //         'keterangan_ditolak_ap' => '', //$request->keterangan_ditolak_ap,
-    //         'status_pengajuan_vendor' => 0, //$request->status_pengajuan_vendor,
-    //         'keterangan_ditolak_vendor' => '', //$request->keterangan_ditolak_vendor,
-    //         'created_by' => auth()->user()->id,
-    //         'updated_by' => auth()->user()->id,
-    //         'created_at' => now(),
-    //         'updated_at' => now(),
-    //     ]);
-
-    //     // Mengelola beberapa item di sini
-    //     $barang_ids = $request->input('barang_id');
-    //     $jumlah = $request->input('jumlah');
-    //     $harga = $request->input('harga');
-
-    //     // Array untuk menyimpan total grand total dari semua item yang dipilih
-    //     $grandTotal = 0;
-
-    //     foreach ($barang_ids as $key => $barang_id) {
-    //         $item_jumlah = $jumlah[$key];
-    //         $item_harga = $harga[$key];
-
-    //         // Menghitung total per item
-    //         $totalPerBarang = $item_jumlah * $item_harga;
-
-    //         // Mengakumulasikan grand total
-    //         $grandTotal += $totalPerBarang;
-
-    //         // Memasukkan detail item ke dalam tabel _detail__pengajuan Anda
-    //         DB::table('_detail__pengajuan')->insert([
-    //             'id_barang' => $barang_id,
-    //             'jumlah' => $item_jumlah,
-    //             'total_per_barang' => $totalPerBarang,
-    //             'id_tr_pengajuan' => $trPengajuanId, // Dengan asumsi Anda memiliki variabel ini
-    //             'created_at' => now(),
-    //             'updated_at' => now(),
-    //         ]);
-
-    //         // Memperbarui stok item yang dipilih (serupa dengan kode Anda yang ada)...
-    //         DB::table('_m_s_t__barang')
-    //         ->where('id', $barang_id)
-    //             ->decrement('stok', $item_jumlah);
-    //     }
-
-    //     // Memperbarui grand total untuk catatan pengajuan utama
-    //     DB::table('_t_r__pengajuan')
-    //     ->where('id', $trPengajuanId)
-    //         ->update(['grand_total' => $grandTotal]);
-
-    //     // Redirect ke rute yang sesuai dengan pesan keberhasilan...
-    //     return redirect()->route('tr_pengajuan')->with('message', 'Pengajuan Barang Berhasil Disimpan!');
-    // }
-
+    
     //transaksi
     public function create()
     {
@@ -224,37 +122,43 @@ class TransaksiBarangController extends Controller
         }
     }
 
-
-    //show barang
-    public function detailpengajuan($id)
+    public function detailpengajuan($id_pengajuan)
     {
-        $transaction = DB::table('_t_r__pengajuan')
-            ->select(
-                '_t_r__pengajuan.*',
-                'created.name as created_by',
-                'updated.name as updated_by',
-                'vendors.nama as vendor_nama', // Select the vendor name
-                '_m_s_t__barang.nama_barang',
-                '_m_s_t__barang.harga',
-                '_m_s_t__barang.satuan',
-                '_detail__pengajuan.jumlah',
-                '_detail__pengajuan.total_per_barang'
-            )
-            ->join('_detail__pengajuan', '_detail__pengajuan.id_tr_pengajuan', '_t_r__pengajuan.id')
-            ->join('users as created', 'created.id', '_t_r__pengajuan.created_by')
-            ->join('users as updated', 'updated.id', '_t_r__pengajuan.updated_by')
-            ->join('_m_s_t__barang', '_m_s_t__barang.id', '_detail__pengajuan.id_barang')
-            ->join('vendors', 'vendors.id', '_m_s_t__barang.vendor_id') // Join with 'vendors' to get vendor name
-            ->where('_t_r__pengajuan.id', $id)
+        $pengajuan = DB::table('_t_r__pengajuan')
+            ->select('_t_r__pengajuan.*', 'name')
+            ->join('users', 'users.id', '=', '_t_r__pengajuan.created_by')
+            ->where('_t_r__pengajuan.id', $id_pengajuan)
             ->first();
 
+        $detailPengajuan = DB::table('_detail__pengajuan')
+            ->select('_detail__pengajuan.*', 'nama_barang', 'harga', 'satuan', 'image', 'deskripsi', 'nama')
+            ->join('_m_s_t__barang', '_m_s_t__barang.id', '=', '_detail__pengajuan.id_barang')
+            ->join('vendors', 'vendors.id', '=', '_m_s_t__barang.vendor_id')
+            ->where('_detail__pengajuan.id_tr_pengajuan', $id_pengajuan)
+            ->get();
+            // dd($detailPengajuan);
+        return view('backend.tr_pengajuan.show', compact('pengajuan', 'detailPengajuan'));
+    }
 
+    public function terimapengajuan($id){
+        DB::table('_t_r__pengajuan')->where('id',$id)->update([
+            'status_pengajuan_ap'=>1
+        ]);
 
-        if (!$transaction) {
-            return redirect()->route('pengajuan')->with('error', 'Transaction not found.');
-        }
+        return redirect()->route('detail_pengajuan',$id)->with('message', 'Pengajuan Berhasil Diajukan');
+    }
 
-        return view('backend.tr_pengajuan.show', compact('transaction'));
+    public function tolakpengajuan($id)
+    {
+        $request = request();
+        $keterangan = $request->input('catatan');
+
+        DB::table('_t_r__pengajuan')->where('id', $id)->update([
+            'status_pengajuan_ap' => 2,
+            'keterangan_ditolak_ap' => $keterangan
+        ]);
+
+        return redirect()->route('detail_pengajuan', $id)->with('message', 'Pengajuan Berhasil Ditolak');
     }
 
     public function deletepengajuan($id)
@@ -281,4 +185,6 @@ class TransaksiBarangController extends Controller
             return redirect()->route('pengajuan')->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
         }
     }
+
+
 }
