cc40ed647202aeac94ef62e13f06d95276051cb9
odiadrian <adrianaodi592@gmail.com>
2023-10-30 17:19:02 +0700

    membuat laporan pdf

---
+++ odi/example-app/resources/views/backend/laporan/laporan1.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laporan Pengajuan</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .kop-surat {
+            width: 100%;
+            text-align: center;
+            padding: 20px 0; /* Padding atas dan bawah */
+        }
+        .surat {
+            margin-top:10px;
+            margin-left:120px;
+            
+        }
+
+        .logo {
+            width: 100px; /* Lebar logo */
+        }
+
+        .judul-kop {
+        text-decoration:underline;
+            font-size: 14px;
+            font-weight: bold;
+        }
+        .nama {
+        text-decoration:underline;
+            font-size: 18px;
+            font-weight: bold;
+        }
+        .nip {
+            font-size: 12px;
+        }
+        .nomor {
+            font-size: 12px;
+        }
+        
+        .alamat-kop {
+            font-size: 14px;
+            margin-top: 10px;
+       
+        }
+        table {
+            width: 100%;
+            text-align: left;
+        }
+        .ttd{
+            font-size: 14px;
+            text-align: right;
+        }
+    </style>
+    
+    <div class="kop-surat">
+        <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Lambang_Kabupaten_Tanggamus.png/661px-Lambang_Kabupaten_Tanggamus.png" alt="Logo Perusahaan"> <!-- Ganti "logo.png" dengan URL atau path gambar logo perusahaan Anda -->
+        
+        <div class="judul-kop">IZIN OPERASIONAL SEKOLAH</div>
+        <div class="nomor">NOMOR:420/1232/03.SK-03/2023</div>
+        <br>
+        <p>DIBERIKAN KEPADA:</p>
+</div>
+
+
+    <div class="surat">
+        <table>
+            <tr>
+                <th>NAMA SEKOLAH</th>
+                <td>:</td>
+                <td>CV Brother Group</td>
+            </tr>
+            <tr>
+                <th>ALAMAT</th>
+                <td>:</td>
+                <td>Jln. M.Kholil RT/RW 001/001</td>
+            </tr>
+            <tr>
+                <th>DESA/KELURAHAN</th>
+                <td>:</td>
+                <td>Lampung</td>
+            </tr>
+            <tr>
+                <th>KECAMATAN</th>
+                <td>:</td>
+                <td>Odi Adrian</td>
+            </tr>
+            <tr>
+                <th>KABUPATEN</th>
+                <td>:</td>
+                <td>Sukanegeri Jaya</td>
+            </tr>
+            <tr>
+                <th>PROVINSI</th>
+                <td>:</td>
+                <td>Lampung</td>
+            </tr>
+            <tr>
+                <th>PENYELENGGARA SEKOLAH</th>
+                <td>:</td>
+                <td>1 November 2023</td>
+            </tr>
+            <tr>
+                <th>PENGESAHAN AKTE NOTARIS</th>
+                <td>:</td>
+                <td>1 November 2023</td>
+            </tr>
+        </table>
+    </div>
+        
+            <p>
+                Dengan Nomor Induk Sekolah <strong>(NIS)</strong> dan Nomor Statistik Sekolah <strong>(NSS)</strong> Sebagai Berikut:
+            </p>
+            <br>
+            <p><strong>NIS:100679</strong></p>
+        <p><strong>NSS:102120718679</strong></p>    
+        <p>
+        <br>
+                Sertifikat ini berlaku  sejak tanggal ditetapkan sampai dengan tanggal 14 Agustus 2026 dan apabila terdapat kekeliruan akan diperbaiki sebagaimana mestinya:
+            </p>
+            <br>
+      <div class="ttd">
+        <p>Sukadana, 14 Agustus 2023</p>
+        
+        <p>KEPALA DINAS PENDIDIKAN DAN KEBUDAYAAN</p>
+        <p>KABUPATEN LAMPUNG TIMUR</p>
+                <p>Hormat saya,</p>
+                <br>
+                <br>
+                <br>
+                <div class="nama">
+                	<strong>MARSAN, S.Pd.Ing</strong>
+                </div>
+                <div class="nip">
+                	pembina Utama Muda
+                    <br>
+                	NIP. 196806101992031015
+				</div>
+            </div>
+      
+    </div>
+</body>
+</html>
